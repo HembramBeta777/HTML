2b8192c3237d10420301b3c3b4060f24e93abd17
Santosh Hembram <santoshhembramis17489@gmail.com>
2022-02-13 21:21:04 +0530

    Add files via upload

---
+++ JS/color button.js
@@ -0,0 +1,13 @@
+function Red()
+{
+	document.body.style.backgroundColor="red";
+}
+function Blue()
+{
+	document.body.style.backgroundColor="blue";
+}
+function Green()
+{
+	document.body.style.backgroundColor="green";
+}
+	
